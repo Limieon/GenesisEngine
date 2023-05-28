d5ece3d63acd99be2578f1e0ea70e9c0cbe3d1f2
Justin Jakob <justin.jakob391@outlook.com>
2023-05-28 22:45:05 +0200

    Rendering many quads onto screen

--- Runtime/src/RuntimeClient/ClientLayer.cpp
+++ Runtime/src/RuntimeClient/ClientLayer.cpp
@@ -21,9 +21,11 @@ namespace runtime {
 		camera.onUpdate(ts);
 
 		ge::client::Renderer2D::begin(camera.getCamera());
-		ge::client::Renderer2D::drawQuad({1.f, 1.f}, {5.f, 5.f}, texture);
-		ge::client::Renderer2D::drawQuad({7.f, 2.f}, {9.f, 7.f}, {1.f, 0.f, 1.f, 1.f});
-		ge::client::Renderer2D::drawQuad({10.f, 14.f}, {12.f, 12.f}, texture, {0.f, 0.f, 1.f, 1.f});
+		for(uint32 x = 0; x < 200; ++x) {
+			for(uint32 y = 0; y < 200; ++y) {
+				ge::client::Renderer2D::drawQuad({x, y}, {0.9f, 0.9f}, {x / 100.f, 1.f, y / 100.f, 1.f});
+			}
+		}
 		ge::client::Renderer2D::end();
 	}
 	void ClientLayer::onDetach() { GE_Info("Client layer detached!"); }
@@ -31,10 +33,9 @@ namespace runtime {
 	void ClientLayer::onEvent(ge::core::Event& e) { camera.onEvent(e); }
 
 	void ClientLayer::onImGUIRender() {
-		bool open = true;
 		auto stats = ge::client::Renderer2D::getStats();
 
-		ImGui::Begin("Renderer2D Stats", &open);
+		ImGui::Begin("Renderer2D Stats", nullptr);
 		ImGui::Text("Draw Calls: %i", (int32) stats.drawCalls);
 		ImGui::Text("Quads: %i", (int32) stats.quadCount);
 		ImGui::Text("Vertices: %i", (int32) stats.getVertexCount());
